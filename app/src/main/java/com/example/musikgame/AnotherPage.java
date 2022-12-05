7b2b7c3ff539179237b10134ac085cd567678e51
Zero1107 <fiqihdiouf70@gmail.com>
2022-12-06 00:33:22 +0700

    Service/Musik Game

---
+++ app/src/main/java/com/example/musikgame/AnotherPage.java
@@ -0,0 +1,13 @@
+package com.example.musikgame;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnotherPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_page);
+    }
+}
